d92fc8bcf08eb64c5acc2b303e9e85f5a90598a1
Priboy313 <62994406+Priboy313@users.noreply.github.com>
2021-01-20 12:33:20 +0300

    Update autopic.js

--- autopic.js
+++ autopic.js
@@ -1,23 +1,7 @@
 	//  https://vk.com/im
 	// write the tagname and press F2
 	//список тегов и ссылки на пикчи
-	var Tags = new Map([
-		['действительно', 'https://sun9-75.userapi.com/impf/c837232/v837232139/599e6/UVsU_o_B5WU.jpg?size=1000x563&quality=96&proxy=1&sign=063b6cae6ba23e5f45b2cd935cda1f9e&type=album'],
-		['нунахер', 'https://sun9-61.userapi.com/impf/c840530/v840530855/3525a/VO7v5o-yAZ0.jpg?size=600x338&quality=96&proxy=1&sign=d8e835d552ba4b39692bf1b98fc466ed&type=album'],
-		['пичот', 'https://sun9-50.userapi.com/impf/8ze-oNod7rliD7UvBfFkJcQ7l6WRobFd1HDAtQ/mVs5gJorNkY.jpg?size=1280x720&quality=96&proxy=1&sign=eab7051ad2a6cfc020660e23129ac161&type=album'],
-		['хех', 'https://sun9-31.userapi.com/impg/DRZqjGZ2R5O49vP34CN6LcSLmxRTK9rziN4aFA/Aorq1demghk.jpg?size=1080x1041&quality=96&proxy=1&sign=2f314d9f966f1a1781fd793b184417ad&type=album'],
-		['дориме', 'https://sun9-61.userapi.com/impg/WA99cnoKYq7UE8neDwsjqQ_5cp-VwarTxOuGpA/madZoCsygko.jpg?size=1080x1065&quality=96&proxy=1&sign=bf5609f76a8a1f8106e526a79c38924c&type=album'],
-		['сделяль', 'https://sun9-42.userapi.com/impf/c841638/v841638657/3082e/eF9dIAFvajs.jpg?size=600x750&quality=96&proxy=1&sign=5811a772d0ed1169b06ff556f5ea851e&type=album'],
-		['ооообля', 'https://sun9-15.userapi.com/impg/w_JYXlAqZ_DD-XDnvElBw81_XTKiZ0NPHO-dKw/9k6vVGSPit0.jpg?size=1080x1080&quality=96&proxy=1&sign=3d23bd6634c29760a08bf94156b52873&type=album'],
-		['курнул', 'https://sun9-76.userapi.com/impg/tqsO2V7PzUspM11kx8r50l_ay1NqTLXZHHVIpg/570rgAiEZrE.jpg?size=1280x851&quality=96&sign=d50875147f4621db8f18835c6a3bdf5e&type=album'],
-		['ушёлвсебя', 'https://sun9-52.userapi.com/impg/R5j2HrCiaFTki_muU5TvnoBGeXYjICsBrfKowQ/uJpVfaiTV-o.jpg?size=500x500&quality=96&proxy=1&sign=6246d04bf50797595fde2c715cf89cdb&type=album'],
-		['хуита', 'https://sun9-34.userapi.com/impg/UhPAKf9P29wA5JE0YjMF4FBSo-53KOfbxzeyGg/-PHL2AL3Ef4.jpg?size=553x570&quality=96&proxy=1&sign=ecce6ed1ba0e5647b52e0098f48d32db&type=album'],
-		['бонжур', 'https://sun9-53.userapi.com/impg/jP0f2Y-oqlVcugQ7QzCQKEhoZ_DiTyO92Bh79Q/E1olQZFCkZ8.jpg?size=884x525&quality=96&proxy=1&sign=cea26a84b8b5d155d96dfe6b9b042d9a&type=album'],
-		['ересь','https://sun9-58.userapi.com/impg/4YbOSw7xH8hS3lHZTDs4MQLP3SvP21bS-wCo2g/RooLByNQOPk.jpg?size=600x400&quality=96&proxy=1&sign=ffe6d003e6239d4b35fa2d20add44bc7&type=album%27'],
-		['адаптируйся', 'https://sun9-54.userapi.com/impg/rTBYVfdTtz9BkNHZN5au8BSSp_-ggfWQKbeNmA/TP1NnrnMqdc.jpg?size=1080x603&quality=96&proxy=1&sign=103775d0c02b769242663d6590f0bc74&type=album'],
-		['богоугодно', 'https://sun9-6.userapi.com/impg/ofW02RS4umG3bedqE4uu4qi16u1ALie09T4CYA/29KhRBmAWYI.jpg?size=604x170&quality=96&proxy=1&sign=0119644e369c9a40fbab683e0165ece5&type=album'],
-		['дублируй_меня_полностью', 'и_поставь_тут_запятую']
-	]); 
+	function AutoPic(){
 	document.addEventListener("keydown", function(event) {
 	if(event.keyCode == 113 && document.activeElement.classList.contains("im-chat-input--text")){
 		var field = document.activeElement;
@@ -102,4 +86,4 @@
 					picwin_block_cont_right.appendChild(cont_but);
 					cont__i++;
 				}
-		}});
+		}});}
